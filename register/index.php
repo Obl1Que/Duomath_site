916d6cb28330720f9ff85cd36cf293fc2a3d36bc
Obl1Que <s.dezhin@mail.ru>
2022-11-06 16:29:20 +0300

    - Added register and login - Added form for register

---
+++ register/index.php
@@ -0,0 +1,9 @@
+<?php
+require '../templates/header.php';
+?>
+
+<form action="index.php" method="post">
+    Login: <input name="login">
+    Password: <input name="password">
+    <input type="submit" value="Register">
+</form>
--- templates/header.php
+++ templates/header.php
@@ -1,2 +1,11 @@
 <title>DuoMath</title>
-<link rel="shortcut icon" href="img/favicon.ico">
+<link rel="shortcut icon" href="/duomath/img/favicon.ico">
+<link rel='stylesheet' href='/duomath/main_css.css'>
+
+<header class="header-plate">
+    <div>
+        <a href="/duomath/">Главная страница</a>
+        <a href="/duomath/register/">Регистрация</a>
+        <a href="/duomath/login/">Автоиризация</a>
+    </div>
+</header>
